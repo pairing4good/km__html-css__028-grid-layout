90d0a0af895724c5826e692d132c320fd03a5eed
David Kessler <39646674+pairing4good@users.noreply.github.com>
2022-11-03 10:35:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 027 Flexbox
+
+[![027 Flexbox](https://img.youtube.com/vi/pGKy92ADipM/0.jpg)](https://www.youtube.com/watch?v=pGKy92ADipM)
+
+The `index.html` file
+- should add styling so that the row displays the items horizontally
+
+Resources:
+- https://www.w3schools.com/css/css3_flexbox.asp
+
+Building toward CSTA Standards:
+- Develop programs for multiple computing platforms. (3B-AP-19) https://www.csteachers.org/page/standards
---
+++ test/verify.test.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the row', () => {
+  it('should display the items horizontally', async () => {
+    const flex = await page.$eval('style', (style) => {
+      return style.innerHTML.match(/\.row.*{[\s\S][^}]*display.*:.*flex.*;/g).length;
+    });
+    
+    expect(flex).toEqual(1);
+  });
+});
